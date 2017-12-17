e9d11d730ab0079449174ee9715d49bda67e0714
Filip Goc <filip@imade3d.com>
2017-12-17 17:10:34 +0100

    fix bed size (2.0 > 1.3)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -801,8 +801,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 180
-#define Y_BED_SIZE 170
+#define X_BED_SIZE 170
+#define Y_BED_SIZE 160
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -20
