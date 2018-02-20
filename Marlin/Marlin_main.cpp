96a01a625d2a190dec73571174fe6db62a315e10
Ian Adams <iadams7@gatech.edu>
2018-02-19 22:32:03 -0500

    Disable Z offset changing in M428

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -9894,7 +9894,7 @@ void quickstop_stepper() {
       }
     }
     
-    LOOP_XYZ(i) if(i = !"z") {set_home_offset((AxisEnum)i, diff[i]);};
+    LOOP_XYZ(i) if(i != 2) {set_home_offset((AxisEnum)i, diff[i]);};
     report_current_position();
     LCD_MESSAGEPGM(MSG_HOME_OFFSETS_APPLIED);
     BUZZ(100, 659);
