d97e92eeadd1d0ddd481c673e1e6ced665fccb67
Ian Adams <iadams7@gatech.edu>
2018-03-07 23:52:52 -0500

    change main menu back message from "Info Screen" to "Go Back"

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -1017,7 +1017,7 @@ void kill_screen(const char* lcd_msg) {
 
   void lcd_main_menu() {
     START_MENU();
-    MENU_BACK(MSG_WATCH);
+    MENU_BACK(MSG_BACK);
 
     #if ENABLED(CUSTOM_USER_MENUS)
       MENU_ITEM(submenu, MSG_USER_MENU, _lcd_user_menu);
