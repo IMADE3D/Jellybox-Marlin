70ec9645f5be4ffafa2a1eb94a9b72358250f84a
Filip Goc <filip@imade3d.com>
2018-03-29 18:18:07 -0400

    make 2.0 size configuration the same as 1.3 (NEEDS more work)

--- Marlin/JellyBOX_configurations/JellyBOX 2.0/JellyBOX_2.0-cold_bed-two_fans/Configuration.h
+++ Marlin/JellyBOX_configurations/JellyBOX 2.0/JellyBOX_2.0-cold_bed-two_fans/Configuration.h
@@ -833,8 +833,8 @@
 #define X_MIN_POS -20
 #define Y_MIN_POS -2
 #define Z_MIN_POS 0
-#define X_MAX_POS 186
-#define Y_MAX_POS 166
+#define X_MAX_POS X_BED_SIZE + 6
+#define Y_MAX_POS Y_BED_SIZE + 6
 #define Z_MAX_POS 146
 
 /**
