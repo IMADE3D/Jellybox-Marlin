c8570a45a9307942abbf2108c238a1a1dfc05ae1
Filip Goc <filip@imade3d.com>
2018-03-30 09:41:45 -0400

    adjust the bed_size in the 2.0 configs (from 1.3 to 2.0 sizing)
    - needs more work! just a rough estimate

--- Marlin/JellyBOX_configurations/JellyBOX 2.0/JellyBOX_2.0-heated_bed-one-fan/Configuration.h
+++ Marlin/JellyBOX_configurations/JellyBOX 2.0/JellyBOX_2.0-heated_bed-one-fan/Configuration.h
@@ -826,11 +826,11 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 170
-#define Y_BED_SIZE 160
+#define X_BED_SIZE 180
+#define Y_BED_SIZE 168
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -20
+#define X_MIN_POS -10
 #define Y_MIN_POS -2
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE + 6
