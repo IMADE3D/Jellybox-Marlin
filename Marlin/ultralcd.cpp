6bb10246108d6a106766aa1c48e0ff4bba23bb82
Ian Adams <iadams7@gatech.edu>
2018-04-06 22:40:20 -0400

    change encoder position to top line when going back

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -616,7 +616,8 @@ uint16_t max_display_update_time = 0;
   void lcd_save_previous_screen() {
     if (screen_history_depth < COUNT(screen_history)) {
       screen_history[screen_history_depth].menu_function = currentScreen;
-      screen_history[screen_history_depth].encoder_position = encoderPosition;
+      //screen_history[screen_history_depth].encoder_position = encoderPosition;
+      encoderPosition = encoderTopLine;
       ++screen_history_depth;
     }
   }
