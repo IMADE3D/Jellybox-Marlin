d028ac1937a5525e8adf16552e6ab0d60c6440d2
Filip Goc <filip@imade3d.com>
2018-04-18 00:47:00 +0200

    put a functional 2.0 config in the root

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1,5 +1,5 @@
 #define JELLYBOX_VARIANT_LINE1 "JellyBOX 2.0"
-#define JELLYBOX_VARIANT_LINE2 "hot bed, two fans"
+#define JELLYBOX_VARIANT_LINE2 "heated bed"
 
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
@@ -35,12 +35,12 @@
 //============================= Thermal Settings ============================
 //===========================================================================
 
-#define TEMP_SENSOR_0 999
+#define TEMP_SENSOR_0 5
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
-#define TEMP_SENSOR_BED 998 //FG Heated Bed
+#define TEMP_SENSOR_BED 1 //FG Heated Bed
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
 #define DUMMY_THERMISTOR_998_VALUE 77
