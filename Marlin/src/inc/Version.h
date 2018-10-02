8fec2092437446d949689b2dc188d8d366d81240
Filip Goc <filip@imade3d.com>
2018-10-03 01:22:22 +0200

    define the jb-3.0.x version

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -38,7 +38,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "bugfix-2.0.x"
+  #define SHORT_BUILD_VERSION "jb-3.0.x-devel"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -73,7 +73,7 @@
   /**
    * Defines a generic printer name to be output to the LCD after booting Marlin.
    */
-  #define MACHINE_NAME "3D Printer"
+  #define MACHINE_NAME "JellyBOX"
 
   /**
    * The SOURCE_CODE_URL is the location where users will find the Marlin Source
@@ -81,7 +81,7 @@
    * has a distinct Github fork— the Source Code URL should just be the main
    * Marlin repository.
    */
-  #define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
+  #define SOURCE_CODE_URL "https://github.com/imade3d"
 
   /**
    * Default generic printer UUID.
@@ -92,6 +92,6 @@
    * The WEBSITE_URL is the location where users can get more information such as
    * documentation about a specific Marlin release.
    */
-  #define WEBSITE_URL "http://marlinfw.org"
+  #define WEBSITE_URL "http://imade3d.com"
 
 #endif // USE_AUTOMATIC_VERSIONING
