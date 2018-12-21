72d20802bff4980790253b1bddbdbdfc21344fc4
Filip Goc <filip@imade3d.com>
2018-12-21 04:48:28 -0500

    Limit the power to the bed 255 -> 170

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -437,7 +437,7 @@
  * When set to any value below 255, enables a form of PWM to the bed that acts like a divider
  * so don't use it unless you are OK with PWM on your bed. (See the comment on enabling PIDTEMPBED)
  */
-#define MAX_BED_POWER 255 // limits duty cycle to bed; 255=full current
+#define MAX_BED_POWER 170 // limits duty cycle to bed; 255=full current
 
 #if ENABLED(PIDTEMPBED)
 
