826d0bed774f558bf977be34ae04c875339ac778
Filip Goc <filip@imade3d.com>
2019-03-20 20:44:23 +0100

    Bump up version number

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -38,7 +38,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "jb-3.0.x-dev.2"
+  #define SHORT_BUILD_VERSION "jb-3.0.x-dev.3"
 
   /**
    * Verbose version identifier which should contain a reference to the location
