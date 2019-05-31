1f88bd5fe71405aadfd8f56851434d5bce099dc6
Filip Goc <filip@imade3d.com>
2019-05-31 17:57:47 +0200

    Sync compilation version number to the marlin

--- Marlin/marlin compile.sh
+++ Marlin/marlin compile.sh
@@ -1,7 +1,7 @@
 #This scripts compiles all the JellyBOX versions with Arduino-cli. Yay. 
 
 ### DEFINE MARLIN VERSION
-marlin_version="jb_marlin_3.0_dev2"
+marlin_version="jbm-3.0-dev4"
 
 ### WHICH JellyBOX VERSIONS TO COMPILE?
 
