83307f27f163571dd08b5d260c0b01cd5084bcb5
Filip Goc <filip@imade3d.com>
2019-08-05 13:57:05 +0200

    Add a dummy Preflight structure.
    Compiles and shows up fine

---
+++ Marlin/src/lcd/menu/menu_prelight_check_imade3d.cpp
@@ -0,0 +1,57 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (C) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "../../inc/MarlinConfig.h"
+
+#include "../../inc/MarlinConfigPre.h"
+#include "menu.h"
+
+#if HAS_LCD_MENU
+
+#include "menu.h"
+#include "../../module/motion.h"
+#include "../../module/planner.h"
+#include "../../module/temperature.h"
+#include "../../Marlin.h"
+
+#if HAS_LEVELING
+  #include "../../feature/bedlevel/bedlevel.h"
+#endif
+
+void menu_preflight_check() {
+  START_MENU();
+  MENU_BACK(MSG_MAIN);
+
+  //
+  // Change filament
+  //
+  MENU_ITEM(gcode, MSG_FILAMENTCHANGE, PSTR("M600 B0"));
+
+  //
+  // Speed:
+  //
+  MENU_ITEM_EDIT(int3, MSG_SPEED, &feedrate_percentage, 10, 999);
+
+  END_MENU(); // menu_preflight_check
+}
+
+#endif //  HAS_LCD_MENU // not yet && ENABLED(PREFLIGHT_CHECK_IMADE3D)
