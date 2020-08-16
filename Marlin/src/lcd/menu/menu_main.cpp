2121aec20dc78d1a753478d23ef98ef70bb8909d
Filip Goc <filip@imade3d.com>
2020-08-16 23:59:36 +0200

    Disable the Change Media menu item
    - we don't use USB so no media switching will be used
    - hard comment-out

--- Marlin/src/lcd/menu/menu_main.cpp
+++ Marlin/src/lcd/menu/menu_main.cpp
@@ -227,13 +227,14 @@ void menu_main() {
 
       if (card_detected) {
         if (!card_open) {
-          MENU_ITEM(gcode,
-            #if PIN_EXISTS(SD_DETECT)
-              MSG_CHANGE_MEDIA, M21_STR
-            #else
-              MSG_RELEASE_MEDIA, PSTR("M22")
-            #endif
-          );
+          // imade3d filip disable; we don't use usb for now
+          // MENU_ITEM(gcode,
+          //   #if PIN_EXISTS(SD_DETECT)
+          //     MSG_CHANGE_MEDIA, M21_STR
+          //   #else
+          //     MSG_RELEASE_MEDIA, PSTR("M22")
+          //   #endif
+          // );
           SUBMENU(MSG_MEDIA_MENU, menu_media);
         }
       }
