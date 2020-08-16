26b8b2975cbc9efed709ab4faff9b9c02c82057d
Filip Goc <filip@imade3d.com>
2020-08-16 23:37:04 +0200

    Use custom Version_imade3d.h file for simplicity

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -125,8 +125,8 @@ BED LEVELING BILINEAR BELOW
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(IMADE3D)" // Who made the changes.
-#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
+#define STRING_CONFIG_H_AUTHOR "IMADE3D" // Who made the changes.
+#define CUSTOM_VERSION_FILE Version_imade3d.h // Path from the root directory (no quotes)
 
 /**
  * *** VENDORS PLEASE READ ***
