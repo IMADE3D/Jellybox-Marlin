8963de27bfed865d10fb42f4fb3ddb981649286b
Filip Goc <filip@imade3d.com>
2020-08-16 23:58:42 +0200

    Re-disable Service menu

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3486,8 +3486,8 @@
 #if ENABLED(PRINTCOUNTER)
   #define SERVICE_WARNING_BUZZES  3
   // Activate up to 3 service interval watchdogs
-  #define SERVICE_NAME_1      "Service. 200 hours printed."
-  #define SERVICE_INTERVAL_1  200 // print hours
+  // #define SERVICE_NAME_1      "Service S"
+  // #define SERVICE_INTERVAL_1  100 // print hours
   //#define SERVICE_NAME_2      "Service L"
   //#define SERVICE_INTERVAL_2  200 // print hours
   //#define SERVICE_NAME_3      "Service 3"
